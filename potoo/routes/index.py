8df90d010a77b163b90bb626037266131bec614d
Benoît Stahl <benasse@users.noreply.github.com>
2020-04-27 19:16:01 +0200

    Update index.py

--- potoo/routes/index.py
+++ potoo/routes/index.py
@@ -4,7 +4,7 @@ from potoo import app
 def index():
         return """
 <h1>Welcome to potoo </h1>
-<p>Potoo is a special bird that communicates with VoIP ecosystem and particularly well with Wazo.</p>
+<p><a href="https://en.wikipedia.org/wiki/Potoo" rel="nofollow">Potoo</a> is a special bird that communicates with VoIP ecosystem and particularly well with Wazo.</p>
 <p>The main objective of this project is to quickly (and perhaps badly) provide solutions to missing functionalities in a given ecosystem.</p>
 <p>Its best documentation is its source code and Potoo may be verry insecure.</p>
 """
