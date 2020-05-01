955444528e5f55fe27c1b4de384656df57471ec4
Benoit Stahl <bs@b5.pm>
2020-05-01 13:57:52 +0200

    pep8

--- potoo/routes/originatev1.py
+++ potoo/routes/originatev1.py
@@ -3,6 +3,7 @@ import configparser
 from potoo import app
 from potoo.utils import *
 
+
 @app.route("/originate/v1")
 def originatev1():
 
@@ -14,17 +15,17 @@ def originatev1():
         src_exten = config['default']['default_originate_number']
         dest_context = config['default']['default_tennant_context']
 
-        try:
-            dest_exten = get_param('dest_exten')
-            dest_context = get_param('dest_context')
-            src_exten = get_param('src_exten')
-            src_context = get_param('src_context')
-        except:
-            pass
-    
-        return '<pre>' + run_originate(dest_context,dest_exten,src_context,src_exten) \
-             + '</pre> dest_exten: ' + dest_exten + '<br> dest_context: ' + dest_context \
-             + '<br> src_exten: ' + src_exten + '<br> src_context: ' + src_context
+        dest_exten = get_param('dest_exten')
+        dest_context = get_param('dest_context')
+        src_exten = get_param('src_exten')
+        src_context = get_param('src_context')
+   
+        if dest_exten:
+            return '<pre>' + run_originate(dest_context,dest_exten,src_context,src_exten) \
+                 + '</pre> dest_exten: ' + dest_exten + '<br> dest_context: ' + dest_context \
+                 + '<br> src_exten: ' + src_exten + '<br> src_context: ' + src_context
+        else:
+            return 'no dest_exten'
     
     else:
         return '401'
