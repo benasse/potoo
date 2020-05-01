a1ade58f4ec2c7753ea6b84a9b8651577c356db1
Benoit Stahl <bs@b5.pm>
2020-05-01 14:23:15 +0200

    pep8

--- potoo/routes/originatev1.py
+++ potoo/routes/originatev1.py
@@ -15,20 +15,22 @@ def originatev1():
         src_exten = config['default']['default_originate_number']
         dest_context = config['default']['default_tennant_context']
 
-        try:   
+        try:
             dest_exten = get_param('dest_exten')
             dest_context = get_param('dest_context')
             src_exten = get_param('src_exten')
             src_context = get_param('src_context')
         except NameError:
             pass
-   
-        try:   
-            return '<pre>' + run_originate(dest_context,dest_exten,src_context,src_exten) \
-                 + '</pre> dest_exten: ' + dest_exten + '<br> dest_context: ' + dest_context \
-                 + '<br> src_exten: ' + src_exten + '<br> src_context: ' + src_context
+
+        try:
+            return '<pre>'
+        + run_originate(dest_context, dest_exten, src_context, src_exten)
+        + '</pre> dest_exten: ' + dest_exten + '<br> dest_context: '
+        + dest_context + '<br> src_exten: ' + src_exten
+        + '<br> src_context: ' + src_context
         except NameError:
             return 'all the params are not set'
-    
+
     else:
         return '401'
